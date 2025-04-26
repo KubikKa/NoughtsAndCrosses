cf8c217a52c7d8a539fbd971caa0ab0a2074b9e9
KubikKa <kubik.katerina.study@gmail.com>
2025-04-26 23:34:41 +0200

    Working on mistakes

--- main.py
+++ main.py
@@ -1,57 +1,40 @@
-"""
-Rated with comments
-[+] - something very positive
-
-[n] - nitpick
-[s] - small issue
-[m] - medium issue
-[!] - major issue
-- Y
-"""
-
 import random
 
 
-# Field for 'Noughts and crosses' game
-"""snake_case for functions, PascalCase for classes [n]"""
-def ClearField():
-    field = [["1","2","3"],["4","5","6"],["7","8","9"]]
-    return field
+# Board for 'Noughts and crosses' game
+def clear_board():
+    board = [["1","2","3"],["4","5","6"],["7","8","9"]]
+    return board
 
 
-# Field styling
+# Display the current board
 """a bit repetitive, could be put in a loop. You can use \n instead of printing black lines [n]"""
-"""unhelpful function name, 'style' could be understood as 'change how it looks', but the function displays the field [n]"""
-def StylingTheField(field):
+def display_board(board):
     print("")
-    print(" " + field[0][0] + " | " + field[0][1] + " | " + field[0][2] + " ")
+    print(" " + board[0][0] + " | " + board[0][1] + " | " + board[0][2] + " ")
     print("---.---.---")
-    print(" " + field[1][0] + " | " + field[1][1] + " | " + field[1][2] + " ")
+    print(" " + board[1][0] + " | " + board[1][1] + " | " + board[1][2] + " ")
     print("---.---.---")
-    print(" " + field[2][0] + " | " + field[2][1] + " | " + field[2][2] + " ")
+    print(" " + board[2][0] + " | " + board[2][1] + " | " + board[2][2] + " ")
     print("")
 
 
-# Changing coordinates of the field's squares into simple numbers
-"""good use of dictionaries [+], but name could be more descriptive (eg. get_coordinates) [n]. Why is is called square?"""
+# Changing coordinates of the board's squares into simple numbers
 """is this function even needed? [s]"""
-def Coordinates(square):
-    numbers = { 
+def get_coordinates(square):
+    numbers = {
         1: [0,0], 2: [0,1], 3: [0,2], 
         4: [1,0], 5: [1,1], 6: [1,2], 
         7: [2,0], 8: [2,1], 9: [2,2]
     }
-    """why use .get() instead of numbers[square]? [n]"""
-    """the difference: [] throws a KeyError, get returns None when key is missing"""
-    return numbers.get(square)
+    return numbers[square]
 
 
 # Winning conditions
-def WhoIsTheWinner(field):
+def who_is_the_winner(board):
     winners = {"X": crosses_player, "O": noughts_player}
     # checking rows
-    """good method for checking rows [+]"""
-    for row in field:
+    for row in board:
         if row[0] == row[1] == row[2] in winners:
             print("%s is the winner!" %winners[row[2]])
             return True
@@ -63,7 +46,7 @@ def WhoIsTheWinner(field):
     you iterate unnecessary number of times, adding overhead
     creating unnecessary variables (columns) [m]
     """
-    columns = [[row[0] for row in field], [row[1] for row in field], [row[2] for row in field]]
+    columns = [[row[0] for row in board], [row[1] for row in board], [row[2] for row in board]]
     """
     The whole for loop here is basically the same as in the part that checks rows, it could be put in a function
     to avoid repetition [n]
@@ -75,36 +58,32 @@ def WhoIsTheWinner(field):
 
     # checking diagonals
     """Could be put into one if statement with the use of 'or' operator [n]"""
-    if field[0][0] == field[1][1] == field[2][2] in winners:
-        print("%s is the winner!" %winners[field[2][2]])
+    if board[0][0] == board[1][1] == board[2][2] in winners:
+        print("%s is the winner!" %winners[board[2][2]])
         return True
 
-    if field[2][0] == field[1][1] == field[0][2] in winners:
-        print("%s is the winner!" %winners[field[0][2]])
+    if board[2][0] == board[1][1] == board[0][2] in winners:
+        print("%s is the winner!" %winners[board[0][2]])
         return True    
     return False
 
 
 # Asking for names and choosing order
-def NameAndOrder():
+def name_and_order():
     while True:
         try:
-            """Why choice2? where's choice1? xd"""
-            choice2 = input("\n> If you want to decide who makes the move, press 1, if not, press 2: ").lower()         
-            if choice2 == "2":
+            choice = input("\n> If you want to decide who makes the move, press 1, if not, press 2: ").lower()         
+            if choice == "2":
                 name1 = input("\nFirst player name: ")
                 name2 = input("Second player name: ")
-                """why define the variables and then put them in a separate list duplicating these variables? [s]"""
-                names = [name1, name2]
                 if name1 == name2:
                     print("> Please, enter two different names to avoid confusion.")
                     continue
-                else:
-                    crosses_player = random.choice(names)
-                    noughts_player = name1 if crosses_player == name2 else name2
-                    return crosses_player, noughts_player
+                crosses_player = random.choice((name1, name2))
+                noughts_player = name1 if crosses_player == name2 else name2
+                return crosses_player, noughts_player
 
-            elif choice2 == "1":
+            elif choice == "1":
                 """Some parts of the code here are repeated from the previous if statement, they could be put in a function [n]"""
                 crosses_player = input("\nCrosses player name: ")
                 noughts_player = input("Noughts player name: ")
@@ -112,20 +91,18 @@ def NameAndOrder():
                     print("> Please, enter two different names to avoid confusion.")
                     """Continue jumps to the next loop, thus executing all of the above code [s]"""
                     continue
-                """inconsistency: you put else: before but not now"""
                 return crosses_player, noughts_player
             
             else:
                 raise ValueError
         
-        except:
-            """except what? [n]"""
+        except ValueError:
             print("\n> Make sure you spell your answer correctly!")
 
 
-# Selection of field squares
-"""This function is too broad. it combines handling input, validating it, field updates, display and logic (bad separation of concerns) [m]"""
-def Move(player_name, computer_name, symbol):
+# Selection of board squares
+"""This function is too broad. it combines handling input, validating it, board updates, display and logic (bad separation of concerns) [m]"""
+def move(player_name, computer_name, symbol):
     while True:
         try:
             if player_name:
@@ -139,9 +116,9 @@ def Move(player_name, computer_name, symbol):
                 until it finds an empty value [s]"""
                 square = random.randint(1, 9)
                 
-            row, column = Coordinates(square)
+            row, column = get_coordinates(square)
 
-            if field[row][column] == "X" or field[row][column] == "O":
+            if board[row][column] == "X" or board[row][column] == "O":
                 if player_name:
                     print("\n> This square is already occupied! Please, choose another one.")
                 """...we are checking if it's players or computers turn every time a player makes a wrong move [n]"""
@@ -149,8 +126,8 @@ def Move(player_name, computer_name, symbol):
             else:
                 if computer_name:
                     print("\n🖥️  Computer chose square number %s." %(square))
-                field[row][column] = symbol   
-                StylingTheField(field)
+                board[row][column] = symbol   
+                display_board(board)
                 break
         except:
             """broad exception again [n]"""
@@ -158,31 +135,28 @@ def Move(player_name, computer_name, symbol):
 
 
 # Choose whether you want to play again
-def PlayAgain():
+def play_again():
     while True:
-        """vague variable name, consider something like 'should_restart' or 'restart_decision' or 'user_choice' [n]"""
-        game = input("\n> Do you want to play again? (Answer Y/N): ").lower()
-        if game == "y":
+        restart_decision = input("\n> Do you want to play again? (Answer Y/N): ").lower()
+        if restart_decision == "y":
             return True 
-        elif game == "n":
+        elif restart_decision == "n":
             return False
-        else:
-            """this else is unnecessary [n]"""
-            print("> Make sure you spell your answer correctly!")
+        print("> Make sure you spell your answer correctly!")
 
 
 # Main game loop
 while True:
     """vague variable names, does it even need to be a separate variable? [n]"""
     text2 = """
-> You can play this game with a friend (press 1) or the computer (press 2). It's up to you 😉
-  Who do you want to play with?
-"""
+    > You can play this game with a friend (press 1) or the computer (press 2). It's up to you 😉
+    Who do you want to play with? 
+    """
     print(text2)
     
     try:
-        field = ClearField()
-        """does it really have to be a separate variable? the field itself already stores this information, you just need to retrieve it [s]"""
+        board = clear_board()
+        """does it really have to be a separate variable? the board itself already stores this information, you just need to retrieve it [s]"""
         available_moves = 9
         """
         try / except should be as small as possible to only handle the part they are concerned about, this whole huge try / except statement
@@ -193,25 +167,24 @@ while True:
         # playing with a friend
         """you put the user selection of name and order in a separate function, why put the choice of gamemode in the main loop? [s]"""
         if choice == 1:
-            crosses_player, noughts_player = NameAndOrder()
-            print("\n> Look, this is your field:")
-            StylingTheField(field)
+            crosses_player, noughts_player = name_and_order()
+            print("\n> Look, this is your board:")
+            display_board(board)
 
             while True:
-                Move(crosses_player, "", "X")
+                move(crosses_player, "", "X")
                 available_moves -= 1
 
-                if WhoIsTheWinner(field):
+                if who_is_the_winner(board):
                     break
                 elif available_moves == 0:
                     print("It's a draw!")
                     break
 
-                Move(noughts_player, "", "O")
+                move(noughts_player, "", "O")
                 available_moves -= 1
 
-                """Good you didn't check for a draw again because it would be unnecessary [+]"""
-                if WhoIsTheWinner(field):
+                if who_is_the_winner(board):
                     break
 
         # playing with the computer
@@ -220,68 +193,55 @@ while True:
                 player_name = input("\nWhat's your name? ")
                 """computer_name is a constant"""
                 computer_name = "Computer"
-                names = [player_name, computer_name]
-                """why define the variables and then put them in a separate list duplicating these variables? [s]"""
                 if player_name == computer_name:
                     print("> Please, enter two different names to avoid confusion.")
                     continue
-                else:
-                    """this else is unnecessary [n]"""
-                    crosses_player = random.choice(names)
-                    noughts_player = player_name if crosses_player == computer_name else computer_name
-                    break
+
+                crosses_player = random.choice((player_name, computer_name))
+                noughts_player = player_name if crosses_player == computer_name else computer_name
+                break
             
             text3 = """> Great, you chose a computer as your opponent!
-  This is your field: 
-"""
+                         This is your board: 
+                    """
             print(text3)
-            StylingTheField(field)
+            display_board(board)
 
             while True:
                 """you are checking which player is playing as what symbol every turn [s]"""
                 if crosses_player == computer_name:
-                    Move("", computer_name, "X")
+                    move("", computer_name, "X")
                 else:
-                    Move(crosses_player, "", "X")
+                    move(crosses_player, "", "X")
                 available_moves -= 1
 
-                if WhoIsTheWinner(field):
+                if who_is_the_winner(board):
                     break
                 elif available_moves == 0:
                     print("It's a draw!")
                     break
 
                 if noughts_player == computer_name:
-                    Move("", computer_name, "O")
+                    move("", computer_name, "O")
                 else:
-                    Move(noughts_player, "", "O")
+                    move(noughts_player, "", "O")
                 available_moves -= 1
             
-                if WhoIsTheWinner(field):
+                if who_is_the_winner(board):
                     break
 
         else:
             raise ValueError
 
-    except:
-        """Except what? [n]"""
+    except ValueError:
         print("> Oops, make sure you chose the right number!")
         continue
 
-    if not PlayAgain():
+    if not play_again():
         break
 
 
 """"
-wnioski:
-[+]: 5
-[n]: 17
-[s]: 9
-[m]: 2
-
-1. tworzysz jakąś strukturę danych i zamiast się jej trzymać, to gdy napotkujesz jakiś problem, konwertujesz ją do innej
-postaci i potem pracujesz na niej zamiast trzymać się tego co już masz.
-
 2. za dużo używania globalnych zmiennych w funkcjach. jest to niebezpieczne. Lepiej przekazywać je funkcjom jako argumenty
 
 guard clauses
